d2f5ea3c2286693c0fe15d2f9b0c78b8612dd092
Wilson-r <wilson.riccardo@iisgalvanimi.edu.it>
2022-06-03 12:01:40 +0000

    nonFUnzia

--- FindYourSchool/src/app/appSettings.ts
+++ FindYourSchool/src/app/appSettings.ts
@@ -1,3 +1,14 @@
+
+
+
 export class AppSettings {
-    public static _API='https://5000-wilsonr-findyourschool-rkl97n6kwrm.ws-eu46.gitpod.io/';
+    //constructor(private  _API2 : any){
+    //var str = new String(process.env["NG_APP_URL"]) 
+    //var splits1 = str.substring(0, 8)
+    //var splits2 = str.substring(8,)
+    //var str3 = splits1+5000+"-"+splits2
+    //this._API2= str3;
+    
+    }
+    //public static _API = this._API2;
  }
